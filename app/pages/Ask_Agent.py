c4dbabe2d350a9d6ca53e60b51ff41575b69d841
VaibhavBansal26 <vaibhav.bansal945@gmail.com>
2025-10-18 14:31:42 -0400

    removed load env

--- app/pages/Ask_Agent.py
+++ app/pages/Ask_Agent.py
@@ -36,8 +36,8 @@
 
 # app/pages/ask_agent.py
 import streamlit as st
-from dotenv import load_dotenv
-load_dotenv()  # load .env for local runs
+# from dotenv import load_dotenv
+# load_dotenv()  # load .env for local runs
 
 from app.utils.agent_client import call_agent
 
